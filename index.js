4ca9832be53ac09566a95c3982495b61c5ff4f3b
Kyle Furry <17813806+racquetmaster@users.noreply.github.com>
2017-08-14 22:48:46 -0500

    Started nms scaffolding

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-console.log("Hello World!");
+let proc = process.argv[2];
+switch(proc){
+    case 'nms':
+        require('./no_mans_sky/index.js');
+        break;
+    case 'test':
+        const sys = require('./botjs/system.js');
 
-const sys = require('./system.js');
-
-sys.wait(5000);
-sys.screenshot(`${__dirname}\\data\\file.png`);
+        sys.wait(5000);
+        sys.screenshot(`${__dirname}\\data\\file.png`);
+        break;
+    default:
+        console.log("No sure which option");
+        break;
+}
